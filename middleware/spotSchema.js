0c4fc08e573f8f3b67cc44eabc5533e2f2c70186
Rosen Ivanov <rosen.ivanov721@gmail.com>
2022-02-13 12:49:09 +0200

    two layer error handling

--- middleware/spotSchema.js
+++ middleware/spotSchema.js
@@ -1,9 +1,7 @@
 const Joi = require('joi');
 
 module.exports.spotSchema = Joi.object({
-    spot: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required()
-    }).required()
 });
