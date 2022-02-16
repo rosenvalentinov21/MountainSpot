effd42b88b6263c051d92b9eb3ad111b131f1645
Rosen Ivanov <rosen.ivanov721@gmail.com>
2022-02-17 00:35:11 +0200

    add passport api , create user model , create user router ,create register and login views.

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const spots = require('./routes/spots');
+const spotRoutes = require('./routes/spots');
+const userRoutes = require('./routes/users');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ExpressError = require('./middleware/expressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/mountain-spot', {
     useNewUrlParser: true,
@@ -17,7 +22,6 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
-
 
 const app = express();
 const port = 3000;
@@ -44,13 +48,21 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/spots', spots);
+app.use('/spots', spotRoutes);
+app.use('/' , userRoutes);
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
@@ -61,6 +73,10 @@ app.use((err, req, res, next) => {
 app.get('/', (req, res) => {
     res.render('index.ejs')
 });
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
 
 app.listen(port , () => {
     console.log(`Listening on port ${port}`);
