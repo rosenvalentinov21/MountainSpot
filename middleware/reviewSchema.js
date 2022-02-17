c3ba8627c23e99db44df436d9c04d2b727117a69
Rosen Ivanov <rosen.ivanov721@gmail.com>
2022-02-17 15:36:28 +0200

    add more lenght validations on spot and review models

--- middleware/reviewSchema.js
+++ middleware/reviewSchema.js
@@ -3,5 +3,5 @@ const { number } = require('joi');
 
 module.exports.reviewSchema = Joi.object({ 
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().min(10).max(50)
 })
--- middleware/spotSchema.js
+++ middleware/spotSchema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 module.exports.spotSchema = Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required()
+        title: Joi.string().required().min(3).max(30),
+        description: Joi.string().required().min(10).max(200),
+        location: Joi.string().min(3).max(30).required()
 });
