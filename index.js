42525a92acf6fa8749eaeef9c9d46e9ba40e3b42
Rosen Ivanov <rosen.ivanov721@gmail.com>
2022-02-23 01:48:45 +0200

    fix serving static files , create home.css to style the homepage

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -75,9 +75,6 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
 
 app.listen(port , () => {
     console.log(`Listening on port ${port}`);
